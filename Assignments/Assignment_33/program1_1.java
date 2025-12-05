243551340029f47232c4ecd0c274f41acdf088d2
AtharvaDhumal2003 <atharvadh2312@gmail.com>
2025-12-05 10:46:49 +0530

    Assignment_33(Q1-Q5)Solutions

---
+++ Assignments/Assignment_33/program1_1.java
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Required Packages
+//  
+///////////////////////////////////////////////////////////////////////////// 
+import java.util.*;
+/////////////////////////////////////////////////////////////////////////////
+// Class Name :     CHARX
+// Function name:   ChkAlpha
+//  Description:    It use to check input is alphabet or not.   
+//  Input:          char
+//  Output:         Boolean
+//  Author:         Atharva Sanjay Dhumal.
+//  Date:           03/12/2025
+//
+/////////////////////////////////////////////////////////////////////////////
+class CHARX
+{
+    Boolean bFlag = false;
+    public Boolean ChkAlpha(char ch)
+    {
+        if(ch >= 'a' && ch <= 'z' || ch >='A' && ch <= 'Z')
+        {
+            bFlag = true;
+        }
+        else
+        {
+            bFlag = false;
+        }
+
+        return bFlag;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////
+//  
+//  Entry Point Function for the application
+//
+/////////////////////////////////////////////////////////////////////////////
+class program1_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        char cValue = '\0';
+        Boolean bRet = false;
+
+        System.out.println("Enter the character:");
+        cValue = sobj.next().charAt(0);
+
+        CHARX cobj = new CHARX();
+        bRet = cobj.ChkAlpha(cValue);
+
+        if(bRet == true)
+        {
+            System.out.println("It is a alphabet");
+        }
+        else
+        {
+            System.out.println("It is not a alphabet");
+        }
+
+
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////
+//
+//Enter the character:
+//q
+//It is a alphabet
+//
+//Enter the character:
+//Q
+//It is a alphabet
+//
+//Enter the character:
+//#
+//It is not a alphabet
+/////////////////////////////////////////////////////////////////////////////////////
